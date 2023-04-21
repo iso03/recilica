089a944eda9295975d122c0cc5dde89514e2f22e
cassoumaila@gmail.com <cassoumaila@gmail.com>
2023-04-21 18:53:08 +0000

    login/register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'loader',
     pathMatch: 'full',
   },
   {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
+    path: 'loader',
+    loadComponent: () => import('./pages/loader/loader.page').then( m => m.LoaderPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
   },
 ];
