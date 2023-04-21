7986bbc172ad91ae126710bef51a7611703864f1
cassoumaila@gmail.com <cassoumaila@gmail.com>
2023-04-21 20:46:06 +0000

    création des pages home, pickup-call et pickup-calls

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,4 +18,16 @@ export const routes: Routes = [
     path: 'register',
     loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'pickup-call',
+    loadComponent: () => import('./pages/pickup-call/pickup-call.page').then( m => m.PickupCallPage)
+  },
+  {
+    path: 'pickup-calls',
+    loadComponent: () => import('./pages/pickup-calls/pickup-calls.page').then( m => m.PickupCallsPage)
+  },
 ];
